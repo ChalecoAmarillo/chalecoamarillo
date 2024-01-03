e5c3c885851fbae6f3df5b415617999bbfb8f46f
ChalecoAmarillo <150099016+ChalecoAmarillo@users.noreply.github.com>
2024-01-03 18:17:32 -0300

    Create masbosteroque.php

---
+++ masbosteroque.php
@@ -0,0 +1,17 @@
+<?php
+   $respuestas = array(
+      "Mas bostero que tener un barbijo de Evita",
+      "Más bostero que tener cortina en vez de puerta",
+      "Mas bostero que guardar el chicle usado en la heladera",
+      "Mas bostero que cagar en la vereda",
+      "Mas bostero que colgase de la luz",
+      "Mas bostero que bajonear puchero frio",
+      "Mas bostero que tunear un fitito",
+      "Mas bostero que mojarse el pelo en un boliche",
+      "Mas bostero que sacar la pelopincho a la vereda",
+      "Mas bostero que culear escuchando Daniel Agostini",
+      "Mas bostero que encararse a la prima en año nuevo"
+       );
+   $random = array_rand($respuestas);
+   echo $respuestas[$random];
+?>

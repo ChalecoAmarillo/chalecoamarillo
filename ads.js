d5f7aa9af30999774c02a6ce6debf21e1bf94436
ChalecoAmarillo <150099016+ChalecoAmarillo@users.noreply.github.com>
2024-02-25 02:35:08 -0300

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,4 +1,8 @@
-var scriptCode = `
-  document.write('<script src="//embed.directvapg.co/miads.js?v:1"></script> ');
-`;
+//var scriptCode = `
+//  document.write('<script src="//apurofutbol.net/assets/js/adsterra.js?v:01"></script> ');
+//`;
 
+var scriptElement = document.createElement('script');
+scriptElement.src = '//decimalmasculineawe.com/6f/b4/ae/6fb4ae4ccbf4dd54c7f8d6ce47dd7353.js';
+scriptElement.type = 'text/javascript';
+document.head.appendChild(scriptElement);
